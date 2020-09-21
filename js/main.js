596864f2193e531b43af3785b553042419af053f
kchrisalis <kayecsmaranan@gmail.com>
2020-09-21 13:03:37 -0600

    3

--- js/main.js
+++ js/main.js
@@ -1,18 +1,28 @@
-// TITLE
+// Snow
 
 // Set up Canvas and Graphics Context
 let cnv = document.getElementById("myCanvas");
 let ctx = cnv.getContext("2d");
 initGraphics(600, 850);
 
-let xVal = [];
-let yVal = [];
-let rVal = [];
-for (let i = 0; i < 10; i++) {
+// Arrays
+let xVal = []; // x value of snow
+let yVal = []; // y value of snow
+let rVal = []; // snow radius
+let sVal = []; // random speed
+
+// Creating Snow
+let snowAmount = 10;
+function snow() {
     xVal.push(Math.randomDec(0, cnv.width));
     yVal.push(Math.randomDec(0, cnv.height));
     rVal.push(Math.randomDec(1, 7));
+    sVal.push(Math.randomDec(1, 5));
 }
+for (let i = 0; i < snowAmount; i++) {
+    snow();
+}
+
 
 // Main Program Loop
 requestAnimationFrame(draw);
@@ -24,19 +34,45 @@ function draw() {
     // Draw Snowflake
     for (let i = 0; i < xVal.length; i++) {
         ctx.fillStyle = "white";
-        ctx.fillCircle(xVal[i], yVal[i], rVal[i]);
+        ctx.fillCircle(xVal[i], yVal[i], rVal[i]); // randomize snow x, y, r
 
-        if (yVal < 850) {
-            yVal++;
+         // random speed (teleport to random spot on canvas once it reaches end of canvas)
+        if (yVal[i] < 850) {
+            yVal[i] += rVal[i];
         } else {
-            yVal = (Math.randomDec(0, cnv.height));
-        } 
+            yVal[i] = (Math.randomDec(-10, 0));
+        }
     }
 
     // Request another Animation Frame
     requestAnimationFrame(draw);
 }
 
-// Each snowflake should have its own location, size and speed.
-// When a snowflake goes off the bottom of the canvas it should teleport to a random location above the canvas.
 // Implement the ability to add and remove snowflakes on mouse or key events.
+
+// Key Events
+let keyCodes = [];
+
+document.addEventListener("keydown", keydownHandler);
+
+function keydownHandler(event) {
+    // Add current keycode to end of array
+    keyCodes.push(event.code);
+    if (keyCodes.length > 4) {
+        keyCodes.shift();
+    }
+
+    if (keyCodes.join() == ["KeyS", "KeyN", "KeyO", "KeyW"].join()) {
+        for (let i = 0; i < 99; i++) {
+            snow();
+        }
+    }
+
+    if (event.code == "KeyR") {
+        xVal = [];
+        yVal = [];
+        rVal = [];
+        sVal = [];
+    }
+}
+
